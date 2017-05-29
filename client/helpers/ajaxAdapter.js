644a156ac29e4e46d724c87b6301166e44e109fd
erin cherney <erin.t.cherney@gmail.com>
2017-05-28 22:09:15 -0400

    window object

--- client/helpers/ajaxAdapter.js
+++ client/helpers/ajaxAdapter.js
@@ -5,21 +5,30 @@ export const ajaxAdapter = {
 //relied on https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch documentation and https://www.npmjs.com/package/fetch-jsonp
 
   getArticles: ()=>{
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/javascript");
+    // const myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "text/javascript");
 
-    const myInit = {
-      headers: myHeaders,
-      mode: 'no-cors'
-    };
+    // const myInit = {
+    //   headers: myHeaders,
+    //   mode: 'no-cors'
+    // };
 
-    return fetchJsonp('http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js', myInit)
-      .then( (r)=>{ return r.json() } )
-      .then((json)=>{
-        console.log('parsed json', json)
-      }).catch((err)=>{
-        console.log('parsing failed', err)
-      })
+    // return fetchJsonp('http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js', myInit)
+    //   .then( (r)=>{ return r.json() } )
+    //   .then((json)=>{
+    //     console.log('parsed json', json)
+    //   }).catch((err)=>{
+    //     console.log('parsing failed', err)
+    //   })
+
+    window["NYTD"]={
+      render_section_front: (data)=>console.log(data)
+    }
+
+    const script = document.createElement('script');
+    script.src = 'http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js?callback=foo'
+
+    document.getElementsByTagName('head')[0].appendChild(script);
   }
 
 }
